69068df6fdfdafbedf11d3cb41270f8ca6c65288
bjhl <bjhl@WHYF-2788.local>
2016-09-30 10:19:09 +0800

    数据库替换代理爬取

---
+++ 爬虫/proxy_change_ontime.py
@@ -0,0 +1,181 @@
+#ecoding:utf-8
+import sys
+import MySQLdb
+import json
+import traceback
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import six
+from six import iteritems
+from six import itervalues
+import mysql.connector
+import os
+class BaseDB:
+
+    '''
+    BaseDB
+    dbcur should be overwirte
+    '''
+    __tablename__ = None
+    placeholder = '%s'
+
+    @staticmethod
+    def escape(string):
+        return '`%s`' % string
+
+    @property
+    def dbcur(self):
+        raise NotImplementedError
+
+    def _execute(self, sql_query, values=[]):
+        dbcur = self.dbcur
+        dbcur.execute(sql_query, values)
+        return dbcur
+
+    def _select(self, tablename=None, what="*", where="", where_values=[], offset=0, limit=None):
+        tablename = self.escape(tablename or self.__tablename__)
+        if isinstance(what, list) or isinstance(what, tuple) or what is None:
+            what = ','.join(self.escape(f) for f in what) if what else '*'
+
+        sql_query = "SELECT %s FROM %s" % (what, tablename)
+        if where:
+            sql_query += " WHERE %s" % where
+        if limit:
+            sql_query += " LIMIT %d, %d" % (offset, limit)
+        #logger.debug("<sql: %s>", sql_query)
+
+        for row in self._execute(sql_query, where_values):
+            yield row
+
+    def _select2dic(self, tablename=None, what="*", where="", where_values=[],
+                    order=None, offset=0, limit=None):
+        tablename = self.escape(tablename or self.__tablename__)
+        if isinstance(what, list) or isinstance(what, tuple) or what is None:
+            what = ','.join(self.escape(f) for f in what) if what else '*'
+
+        sql_query = "SELECT %s FROM %s" % (what, tablename)
+        if where:
+            sql_query += " WHERE %s" % where
+        if order:
+            sql_query += ' ORDER BY %s' % order
+        if limit:
+            sql_query += " LIMIT %d, %d" % (offset, limit)
+        #logger.debug("<sql: %s>", sql_query)
+
+        dbcur = self._execute(sql_query, where_values)
+        fields = [f[0] for f in dbcur.description]
+
+        for row in dbcur:
+            yield dict(zip(fields, row))
+
+    def _replace(self, tablename=None, **values):
+        tablename = self.escape(tablename or self.__tablename__)
+        if values:
+            _keys = ", ".join(self.escape(k) for k in values)
+            _values = ", ".join([self.placeholder, ] * len(values))
+            sql_query = "REPLACE INTO %s (%s) VALUES (%s)" % (tablename, _keys, _values)
+        else:
+            sql_query = "REPLACE INTO %s DEFAULT VALUES" % tablename
+        #logger.debug("<sql: %s>", sql_query)
+
+        if values:
+            dbcur = self._execute(sql_query, list(itervalues(values)))
+        else:
+            dbcur = self._execute(sql_query)
+        return dbcur.lastrowid
+
+    def _insert(self, tablename=None, **values):
+        tablename = self.escape(tablename or self.__tablename__)
+        if values:
+            _keys = ", ".join((self.escape(k) for k in values))
+            _values = ", ".join([self.placeholder, ] * len(values))
+            sql_query = "INSERT INTO %s (%s) VALUES (%s)" % (tablename, _keys, _values)
+        else:
+            sql_query = "INSERT INTO %s DEFAULT VALUES" % tablename
+        #logger.debug("<sql: %s>", sql_query)
+
+        if values:
+            dbcur = self._execute(sql_query, list(itervalues(values)))
+        else:
+            dbcur = self._execute(sql_query)
+        return dbcur.lastrowid
+
+    def _update(self, tablename=None, where="1=0", where_values=[], **values):
+        tablename = self.escape(tablename or self.__tablename__)
+        _key_values = ", ".join([
+            "%s = %s" % (self.escape(k), self.placeholder) for k in values
+        ])
+        sql_query = "UPDATE %s SET %s WHERE %s" % (tablename, _key_values, where)
+        #print ("<sql: %s>", sql_query)
+
+        return self._execute(sql_query, list(itervalues(values)) + list(where_values))
+
+    def _delete(self, tablename=None, where="1=0", where_values=[]):
+        tablename = self.escape(tablename or self.__tablename__)
+        sql_query = "DELETE FROM %s" % tablename
+        if where:
+            sql_query += " WHERE %s" % where
+        #logger.debug("<sql: %s>", sql_query)
+
+        return self._execute(sql_query, where_values)
+
+
+class DB(BaseDB):
+    __tablename__ = "csdn_proxy2"
+
+    def __init__(self):
+        config = {'host': '127.0.0.1',  # 默认127.0.0.1
+                  'user': 'root',
+                  'password': '123',
+                  'port': 3306,  # 默认即为3306
+                  'database': 'taskdb',
+                  'charset': 'utf8' ,
+                  'autocommit' : True
+                  }
+
+        self.conn = mysql.connector.connect(**config)
+        self.database_name = 'taskdb'
+
+    @property
+    def dbcur(self):
+        try:
+            if self.conn.unread_result:
+                self.conn.get_rows()
+            return self.conn.cursor()
+        except (mysql.connector.OperationalError, mysql.connector.InterfaceError):
+            self.conn.ping(reconnect=True)
+            self.conn.database = self.database_name
+            return self.conn.cursor()
+
+
+
+db = DB()
+#update_db = DB()
+what = ['taskid','fetch']
+import urllib2,time
+
+ip_list = [
+	'124.88.67.7:843',
+	'119.188.94.145:80',
+	'183.19.12.244:3128',
+	'1.160.1.56:8080',
+	'125.39.68.70:83',
+]
+while True:
+        for line in ip_list:
+            for each in list(db._select2dic(what=what)):
+                if each['fetch']:
+                    _dict = json.loads(str(each['fetch']))
+                    print 'old_ip: '+_dict['proxy']
+                    _dict['proxy'] = line
+                    print 'new_ip: '+_dict['proxy']
+                    new_fetch = json.dumps(_dict)
+                    update_dict = {}
+                    update_dict['fetch'] = new_fetch
+                    where = '`taskid` = %s' % db.placeholder
+                    where_values = [str(each['taskid']),]
+                    try:
+                        db._update(where = where, where_values = where_values, **update_dict)
+                    except Exception as ee:
+                        print ee.message
+            time.sleep(600)
